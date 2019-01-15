497269daa653e51ba298f656bd809337124865a6
Gremious <gremious@protonmail.com>
2019-01-15 05:00:48 +0000

    Mask synergy works!!!

--- theThief/src/main/java/thiefmod/patches/ThievesMaskPatch.java
+++ theThief/src/main/java/thiefmod/patches/ThievesMaskPatch.java
@@ -7,7 +7,7 @@ import com.megacrit.cardcrawl.relics.SmilingMask;
 import com.megacrit.cardcrawl.shop.OnSaleTag;
 import com.megacrit.cardcrawl.shop.ShopScreen;
 import javassist.CtBehavior;
-import thiefmod.relics.*;
+import thiefmod.relics.ThievesMask;
 
 /*
 @SpirePatch(
@@ -29,25 +29,26 @@ public class ThievesMaskPatch {
 }
 */
 
-
 @SpirePatch(
         clz = ShopScreen.class,
         method = "initCards"
 )
 
 public class ThievesMaskPatch {
+    @SpireInsertPatch(
+            locator = Locator.class,
+            localvars = {"saleCard"}
+    )
 
     public static void insert(ShopScreen __instance, @ByRef AbstractCard[] saleCard) {
-    //   if (AbstractDungeon.player.hasRelic(SmilingMask.ID) && AbstractDungeon.player.hasRelic(ThievesMask.ID)) {
+        if (AbstractDungeon.player.hasRelic(SmilingMask.ID) && AbstractDungeon.player.hasRelic(ThievesMask.ID)) {
             saleCard[0].price = 0;
-    //}
+        }
     }
 
-    private static class Locator extends SpireInsertLocator
-    {
+    private static class Locator extends SpireInsertLocator {
         @Override
-        public int[] Locate(CtBehavior ctMethodToPatch) throws Exception
-        {
+        public int[] Locate(CtBehavior ctMethodToPatch) throws Exception {
             Matcher finalMatcher = new Matcher.NewExprMatcher(OnSaleTag.class);
             return LineFinder.findInOrder(ctMethodToPatch, finalMatcher);
         }
