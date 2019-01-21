4d4295cdffae8dd59924febdbdcaac5ff7ee343d
Gremious <gremious@protonmail.com>
2019-01-21 14:59:51 +0000

    Hoodlum done

--- theThief/src/main/java/thiefmod/cards/Hoodlum.java
+++ theThief/src/main/java/thiefmod/cards/Hoodlum.java
@@ -1,27 +1,17 @@
 package thiefmod.cards;
 
 import basemod.helpers.TooltipInfo;
-import com.evacipated.cardcrawl.mod.stslib.fields.cards.AbstractCard.FleetingField;
-import com.evacipated.cardcrawl.mod.stslib.variables.ExhaustiveVariable;
-import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
-import com.megacrit.cardcrawl.actions.common.DamageAction;
-import com.megacrit.cardcrawl.actions.common.GainBlockAction;
-import com.megacrit.cardcrawl.actions.common.MakeTempCardInDrawPileAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
-import com.megacrit.cardcrawl.cards.DamageInfo;
-import com.megacrit.cardcrawl.cards.status.VoidCard;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.localization.CardStrings;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
-import com.megacrit.cardcrawl.powers.VulnerablePower;
 import thiefmod.ThiefMod;
-import thiefmod.actions.common.StealCardAction;
 import thiefmod.patches.Character.AbstractCardEnum;
 import thiefmod.patches.Unique.ThiefCardTags;
-import thiefmod.powers.Common.ShadowstepPower;
+import thiefmod.powers.Unique.HoodlumPower;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -50,37 +40,17 @@ public class Hoodlum extends AbstractBackstabCard {
     private static final CardType TYPE = CardType.POWER;
 
     private static final int COST = 1;
-    private static final int UPGRADE_COST = 0;
-
-    private static final int DAMAGE = 6;
-    private static final int UPGRADE_PLUS_DAMAGE = 3;
-
-    private static final int BLOCK = 6;
-    private static final int UPGRADE_PLUS_BLOCK = 3;
 
     private static final int MAGIC = 1;
-    private static final int UPGRADED_PLUS_MAGIC = 1;
-
-    private static final int BACKSTAB = 2;
-    private static final int UPGRADED_PLUS_BACKSTAB = 1;
-
-    private static final String ADD_LOCATION = "Hand"; // If stolen card.
-    private static final boolean ADD_RANDOM = true;
-    private static final boolean ADD_UPGRADED = false;
 
 // /STAT DECLARATION/
 
     public Hoodlum() {
         super(ID, NAME, IMG, COST, DESCRIPTION, TYPE, COLOR, RARITY, TARGET);
 
-        ExhaustiveVariable.setBaseValue(this, 2);
+        this.isInnate = this.upgraded;
 
-        FleetingField.fleeting.set(this, true);
-        this.baseDamage = DAMAGE;
         this.magicNumber = this.baseMagicNumber = MAGIC;
-        this.baseBlock = BLOCK;
-        this.backstabNumber = this.baseBackstabNumber = BACKSTAB;
-
 
         this.tags.add(ThiefCardTags.BACKSTAB);
         this.tags.add(ThiefCardTags.SHADOWSTEP);
@@ -102,85 +72,10 @@ public class Hoodlum extends AbstractBackstabCard {
     // Actions the card should do.
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
-        final int count = AbstractDungeon.actionManager.cardsPlayedThisTurn.size();
+        AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(p, p, new HoodlumPower(p, p, this.magicNumber), this.magicNumber));
 
-        if (count <= 1) {
-            AbstractDungeon.actionManager.addToBottom(new DamageAction(
-                    m, new DamageInfo(p, this.damage * this.backstabNumber, this.damageTypeForTurn),
-                    AbstractGameAction.AttackEffect.SLASH_VERTICAL));
-        } else {
-            AbstractDungeon.actionManager.addToBottom(new StealCardAction(
-                    p, this.magicNumber, 1, ADD_RANDOM, true, ADD_LOCATION, ADD_UPGRADED));
-        }
-
-        AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(
-                p, p, new ShadowstepPower(
-                p, p, this.magicNumber), this.magicNumber));
-
-        AbstractDungeon.actionManager.addToBottom(new GainBlockAction(
-                p, p, this.block));
-
-        while (this.backstabNumber-- != 0) {
-            AbstractDungeon.actionManager.addToBottom(
-                    new MakeTempCardInDrawPileAction(new VoidCard(), this.backstabNumber, true, true, false));
-        }
-
-        for (AbstractMonster mo : AbstractDungeon.getCurrRoom().monsters.monsters) {
-            AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(mo, p, new VulnerablePower(
-                    mo, this.magicNumber, false), this.magicNumber));
-        }
-
-        this.backstabNumber = this.baseBackstabNumber;
     }
 
-    /*
-
-    @Override // Startup: Add 1 void to your draw pile.
-    public boolean atBattleStartPreDraw() {
-        AbstractDungeon.actionManager.addToBottom(
-                new MakeTempCardInDrawPileAction(new VoidCard(), this.magicNumber, true, true, false));
-        return true;
-    }
-
-    */
-
-    @Override
-    public void applyPowers() {
-        super.applyPowers();
-
-        if (this.magicNumber >= 2) {
-            this.rawDescription = UPGRADE_DESCRIPTION;
-        } else {
-            this.rawDescription = DESCRIPTION;
-        }
-
-        if (AbstractDungeon.player.cardsPlayedThisTurn == 0) {
-            this.rawDescription += this.EXTENDED_DESCRIPTION[1];
-        } else {
-            this.rawDescription += this.EXTENDED_DESCRIPTION[2];
-        }
-        this.initializeDescription();
-    }
-
-    /*
-        @Override
-    public void optionSelected(AbstractPlayer p, AbstractMonster m, int i)
-    {
-        switch (i) {
-            case 0:
-                 AbstractDungeon.actionManager.addToTop(new FetchAction(AbstractDungeon.player.drawPile, this.magicNumber));
-                break;
-            case 1:
-                AbstractDungeon.actionManager.addToTop(new FetchAction(AbstractDungeon.player.discardPile, this.magicNumber));
-                break;
-            case 2:
-                AbstractDungeon.actionManager.addToTop(new FetchAction(AbstractDungeon.player.exhaustPile, this.magicNumber));
-                break;
-            default:
-                return;
-        }
-    }
-     */
     @Override
     public List<TooltipInfo> getCustomTooltips() {
         List<TooltipInfo> tips = new ArrayList<>();
@@ -200,13 +95,8 @@ public class Hoodlum extends AbstractBackstabCard {
     public void upgrade() {
         if (!this.upgraded) {
             this.upgradeName();
-            this.upgradeBaseCost(UPGRADE_COST);
-            this.upgradeMagicNumber(UPGRADED_PLUS_MAGIC);
-            this.upgradeDamage(UPGRADE_PLUS_DAMAGE);
-            this.upgradeBlock(UPGRADE_PLUS_BLOCK);
-            this.upgradeBackstabNumber(UPGRADED_PLUS_BACKSTAB);
             this.isInnate = true;
-//          this.rawDescription = UPGRADE_DESCRIPTION;
+            this.rawDescription = UPGRADE_DESCRIPTION;
             this.initializeDescription();
         }
     }
--- theThief/src/main/java/thiefmod/powers/Unique/HoodlumPower.java
+++ theThief/src/main/java/thiefmod/powers/Unique/HoodlumPower.java
@@ -1,25 +1,30 @@
 package thiefmod.powers.Unique;
 
 import com.badlogic.gdx.graphics.Texture;
+import com.megacrit.cardcrawl.actions.common.DiscardAction;
+import com.megacrit.cardcrawl.actions.common.DrawCardAction;
 import com.megacrit.cardcrawl.actions.common.ReducePowerAction;
 import com.megacrit.cardcrawl.actions.common.RemoveSpecificPowerAction;
+import com.megacrit.cardcrawl.actions.utility.UseCardAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.core.AbstractCreature;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.localization.PowerStrings;
 import com.megacrit.cardcrawl.powers.AbstractPower;
 import thiefmod.ThiefMod;
+import thiefmod.patches.Unique.ThiefCardTags;
 
 // Empty Base
 
 public class HoodlumPower extends AbstractPower {
     public AbstractCreature source;
 
-	public static final String POWER_ID = ThiefMod.makeID("Empty");
-	private static final PowerStrings powerStrings = CardCrawlGame.languagePack.getPowerStrings(POWER_ID);
-	public static final String NAME = powerStrings.NAME;
-	public static final String[] DESCRIPTIONS = powerStrings.DESCRIPTIONS;
-	public static final String IMG = ThiefMod.makePath(ThiefMod.COMMON_POWER);
+    public static final String POWER_ID = ThiefMod.makeID("Empty");
+    private static final PowerStrings powerStrings = CardCrawlGame.languagePack.getPowerStrings(POWER_ID);
+    public static final String NAME = powerStrings.NAME;
+    public static final String[] DESCRIPTIONS = powerStrings.DESCRIPTIONS;
+    public static final String IMG = ThiefMod.makePath(ThiefMod.COMMON_POWER);
 
 
     public HoodlumPower(AbstractCreature owner, AbstractCreature source, final int amount) {
@@ -28,7 +33,7 @@ public class HoodlumPower extends AbstractPower {
         this.img = new Texture(IMG);
         this.type = PowerType.BUFF;
         this.isTurnBased = false;
-        
+
         this.owner = owner;
         this.source = source;
 
@@ -37,10 +42,22 @@ public class HoodlumPower extends AbstractPower {
         this.updateDescription();
     }
 
-	@Override
-	public void atStartOfTurn() {
+    @Override
+    public void onUseCard(AbstractCard card, UseCardAction action) {
+        if (card.hasTag(ThiefCardTags.BACKSTAB)) {
+            if (AbstractDungeon.actionManager.cardsPlayedThisTurn.size() <= 0) {
+                this.flash();
+                AbstractDungeon.actionManager.addToBottom(new DrawCardAction(this.owner, this.amount));
+                AbstractDungeon.actionManager.addToBottom(new DiscardAction(this.owner, this.owner, this.amount, false));
 
-	}
+            }
+        } else if (card.hasTag(ThiefCardTags.STOLEN)) {
+            this.flash();
+            AbstractDungeon.actionManager.addToBottom(new DrawCardAction(this.owner, this.amount));
+            AbstractDungeon.actionManager.addToBottom(new DiscardAction(this.owner, this.owner, this.amount, false));
+        }
+
+    }
 
     public void atEndOfRound() {
         if (this.amount == 0) {
@@ -53,15 +70,14 @@ public class HoodlumPower extends AbstractPower {
 
     // Update the description when you apply this power. (i.e. add or remove an "s" in keyword(s))
     @Override
-    public void updateDescription() 
-    {
-    	if (this.amount == 1){
-    		this.description = DESCRIPTIONS[0] + this.amount + DESCRIPTIONS[1];}
-    	
-    	else if (this.amount > 1) {
-    		this.description = DESCRIPTIONS[0] + this.amount + DESCRIPTIONS[2];}
-    	}
-    
+    public void updateDescription() {
+        if (this.amount == 1) {
+            this.description = DESCRIPTIONS[0] + this.amount + DESCRIPTIONS[1];
+        } else if (this.amount > 1) {
+            this.description = DESCRIPTIONS[0] + this.amount + DESCRIPTIONS[2];
+        }
+    }
+
 
 }
 
