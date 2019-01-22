339d6ef8550e884c0c3c6a8446a1539fff32e37e
Gremious <gremious@protonmail.com>
2019-01-22 22:40:03 +0000

    slow commit i've been trying too hard to make this card work it still doesn't but i'm tryin

---
+++ theThief/src/main/java/thiefmod/actions/unique/RetrievalAction.java
@@ -0,0 +1,51 @@
+package thiefmod.actions.unique;
+
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.utility.DiscardToHandAction;
+import com.megacrit.cardcrawl.actions.utility.WaitAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import thiefmod.ThiefMod;
+import thiefmod.actions.common.DrawPileToHandAction;
+import thiefmod.actions.common.ExhaustToHandAction;
+
+import java.util.ArrayList;
+
+public class RetrievalAction extends AbstractGameAction {
+    private ArrayList<AbstractCard> cardsToReturn;
+
+    private static final Logger logger = LogManager.getLogger(ThiefMod.class.getName());
+
+    public RetrievalAction(ArrayList<AbstractCard> cardsToReturn) {
+        this.cardsToReturn = cardsToReturn;
+    }
+
+    @Override
+    public void update() {
+
+        logger.info("Update start");
+        AbstractDungeon.actionManager.addToBottom(new WaitAction(0.1f));
+        AbstractDungeon.actionManager.addToBottom(new WaitAction(0.1f));
+        AbstractDungeon.actionManager.addToBottom(new WaitAction(0.1f));
+        logger.info("Waited out");
+
+        for (AbstractCard iterateCard : cardsToReturn) {
+            logger.info("For loop started");
+          /*
+           for (AbstractCard cardCheckDiscard : AbstractDungeon.player.discardPile.group) {
+                if (cardCheckDiscard == iterateCard) {}
+            }
+          */
+            AbstractDungeon.actionManager.addToBottom(new DiscardToHandAction(iterateCard));
+            logger.info("Discard to hand added.");
+            AbstractDungeon.actionManager.addToBottom(new DrawPileToHandAction(iterateCard));
+            logger.info("Draw to hand added.");
+            AbstractDungeon.actionManager.addToBottom(new ExhaustToHandAction(iterateCard));
+            logger.info("Exhaust to hand added.");
+        }
+        logger.info("RetrievalAction is done.");
+        this.isDone = true;
+    }
+}
--- theThief/src/main/java/thiefmod/powers/Unique/RetrievalPower.java
+++ theThief/src/main/java/thiefmod/powers/Unique/RetrievalPower.java
@@ -1,24 +1,28 @@
 package thiefmod.powers.Unique;
 
-import com.badlogic.gdx.graphics.Texture;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.core.AbstractCreature;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.helpers.ImageMaster;
 import com.megacrit.cardcrawl.localization.PowerStrings;
-import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import com.megacrit.cardcrawl.powers.AbstractPower;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import thiefmod.ThiefMod;
+import thiefmod.actions.unique.RetrievalAction;
 
 import java.util.ArrayList;
 import java.util.Collections;
 
 
 public class RetrievalPower extends AbstractPower {
+    private static final Logger logger = LogManager.getLogger(ThiefMod.class.getName());
+    public ArrayList<AbstractCard> lastCardsPlayed;
 
     public AbstractCreature source;
-    public int returnAmount;
-    static int count = AbstractDungeon.actionManager.cardsPlayedThisTurn.size();
+    private int returnAmount;
+    private int count = 0;
 
     public static final String POWER_ID = ThiefMod.makeID("RetrievalPower");
     private static final PowerStrings powerStrings = CardCrawlGame.languagePack.getPowerStrings(POWER_ID);
@@ -26,14 +30,13 @@ public class RetrievalPower extends AbstractPower {
     public static final String[] DESCRIPTIONS = powerStrings.DESCRIPTIONS;
     public static final String IMG = ThiefMod.makePath(ThiefMod.COMMON_POWER);
 
-    public static ArrayList<AbstractCard> playedCards;
 
     public RetrievalPower(final AbstractCreature owner, final AbstractCreature source, final int amount, final int returnAmount) {
 
         this.name = NAME;
         this.ID = POWER_ID;
 
-        this.img = new Texture(IMG);
+        this.img = ImageMaster.loadImage(IMG);
         this.type = PowerType.BUFF;
         this.isTurnBased = false;
 
@@ -47,41 +50,107 @@ public class RetrievalPower extends AbstractPower {
         this.updateDescription();
     }
 
+
     @Override
-    public void onPlayCard(AbstractCard onPlayedCard, AbstractMonster onPlayedMonster) {
-        this.updateDescription();
-        if (count == 3) {
+    public void onAfterCardPlayed(AbstractCard cardPlayed) {
+        logger.info("Ayo a " + cardPlayed + " was just played." +
+                " Currently, count is " + count + " and the whole list is " + lastCardsPlayed);
+        count++;
+        lastCardsPlayed.add(cardPlayed);
 
-            playedCards = AbstractDungeon.actionManager.cardsPlayedThisCombat;
-            Collections.reverse(playedCards);
+        logger.info("Update: the card that was played is still " + cardPlayed +
+                "Count++ is " + count + " and the whole list is " + lastCardsPlayed);
 
-            for (int i = 0; i < returnAmount; i++) {
-                for (AbstractCard card : playedCards) {
-                    if (card == null) {
-                        break;
-                    } else {
-
-                        AbstractDungeon.player.hand.addToHand(card);
-                        playedCards.remove(card);
-
-                    }
-                    break;
-                }
-                playedCards.clear();
-
-                this.updateDescription();
-            }
+        if (count == 3) ;
+        {logger.info("Count is 3, do your thing.");
+            Collections.reverse(lastCardsPlayed);
+            logger.info("lastCardsPlayed reversed is: " + lastCardsPlayed);
+            AbstractDungeon.actionManager.addToBottom(new RetrievalAction(lastCardsPlayed));
+            logger.info("Action completed.");
+            lastCardsPlayed.clear();
+            logger.info("Cards cleared.");
         }
     }
 
+        /*
+        logger.info("onAfterCardPlayedStart");
+        lastCardsPlayed.add(cardPlayed);
 
+        Collections.reverse(lastCardsPlayed);
+
+        logger.info("Currently, lastCardsPlayed contains: " + lastCardsPlayed);
+        logger.info("Currently, there have been " + lastCardsPlayed.size() + " cards played");
+
+        for (AbstractCard listCard : lastCardsPlayed) {
+
+            logger.info(listCard);
+
+            for (AbstractCard cardCheckDiscard : AbstractDungeon.player.discardPile.group) {
+
+                logger.info("If loop checking if : " + cardCheckDiscard.uuid + " == " + listCard.uuid);
+                if (cardCheckDiscard.uuid == listCard.uuid) {
+
+                    logger.info("It is! Add " + listCard + " to hand");
+
+                    AbstractDungeon.actionManager.addToBottom(new DiscardToHandAction(listCard));
+
+                    // AbstractDungeon.player.discardPile.addToHand(listCard); - Doesn't work.
+
+                    logger.info("remove listCard from lastCardsPlayed and then break.");
+                    break;
+                }
+            }
+        }
+*/
+
+
+    /* @Override
+     public void onPlayCard(AbstractCard onPlayedCard, AbstractMonster onPlayedMonster) {
+         this.updateDescription();
+
+             playedCards = AbstractDungeon.actionManager.cardsPlayedThisCombat;
+             Collections.reverse(playedCards);
+
+             for (int i = 0; i < returnAmount; i++) {
+                 for (AbstractCard card : playedCards) {
+                     if (card == null) {
+                         break;
+                     } else {
+
+                         for (AbstractCard cardCheckDraw : AbstractDungeon.player.drawPile.group)
+                             if (cardCheckDraw.uuid == card.uuid) {
+                                 AbstractDungeon.player.drawPile.addToHand(card);
+                                 playedCards.remove(card);
+                                 break;
+                             }
+
+                         for (AbstractCard cardCheckDraw : AbstractDungeon.player.discardPile.group)
+                             if (cardCheckDraw.uuid == card.uuid) {
+                                 AbstractDungeon.player.discardPile.addToHand(card);
+                                 playedCards.remove(card);
+                                 break;
+                             }
+
+                         for (AbstractCard cardCheckDraw : AbstractDungeon.player.exhaustPile.group)
+                             if (cardCheckDraw.uuid == card.uuid) {
+                                 AbstractDungeon.player.exhaustPile.addToHand(card);
+                                 playedCards.remove(card);
+                                 break;
+                             }
+                     }
+                     break;
+                 }
+                 playedCards.clear();
+
+                 this.updateDescription();
+             }
+
+     }
+ */
     @Override
     public void updateDescription() {
-        if (count <3)
-            this.description = DESCRIPTIONS[0] + returnAmount + DESCRIPTIONS[1] + count + DESCRIPTIONS[2];
-        else{
-            this.description = DESCRIPTIONS[3] + returnAmount + DESCRIPTIONS[4];
-        }
+        this.description = DESCRIPTIONS[0];
+
     }
 
 }
