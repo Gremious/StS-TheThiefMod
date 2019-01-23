28d6ac63cd053bf747720bb414a290ab59cac220
Gremious <gremious@protonmail.com>
2019-01-23 20:45:28 +0000

    Gotta fix *every* Backstab card now

--- theThief/src/main/java/thiefmod/cards/backstab/AttackOfOpportunity.java
+++ theThief/src/main/java/thiefmod/cards/backstab/AttackOfOpportunity.java
@@ -4,8 +4,7 @@ import basemod.helpers.TooltipInfo;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.actions.common.DamageAction;
 import com.megacrit.cardcrawl.actions.common.DrawCardAction;
-import com.megacrit.cardcrawl.actions.common.MakeTempCardInDiscardAction;
-import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.actions.common.MakeTempCardInDrawPileAction;
 import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.cards.status.VoidCard;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
@@ -17,6 +16,8 @@ import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import thiefmod.ThiefMod;
 import thiefmod.cards.AbstractBackstabCard;
 import thiefmod.patches.Character.AbstractCardEnum;
+import thiefmod.patches.Unique.ThiefCardTags;
+import thiefmod.powers.Common.BackstabPower;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -24,20 +25,24 @@ import java.util.List;
 public class AttackOfOpportunity extends AbstractBackstabCard {
 
 
-// TEXT DECLARATION
+    // TEXT DECLARATION
 
     public static final String ID = ThiefMod.makeID("AttackOfOpportunity");
     public static final String IMG = ThiefMod.makePath(ThiefMod.DEFAULT_UNCOMMON_ATTACK);
     public static final CardColor COLOR = AbstractCardEnum.THIEF_GRAY;
+
     private static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
     private static final UIStrings uiStrings = CardCrawlGame.languagePack.getUIString("FlavorText");
-    public static final String FLAVOR_STRINGS[] = uiStrings.TEXT;
+
     public static final String NAME = cardStrings.NAME;
     public static final String DESCRIPTION = cardStrings.DESCRIPTION;
+
+    public static final String FLAVOR_STRINGS[] = uiStrings.TEXT;
     public static final String EXTENDED_DESCRIPTION[] = cardStrings.EXTENDED_DESCRIPTION;
 
 
-// /TEXT DECLARATION/
+    // /TEXT DECLARATION/
+
 
     // STAT DECLARATION
     public static final String UPGRADE_DESCRIPTION = cardStrings.UPGRADE_DESCRIPTION;
@@ -49,14 +54,15 @@ public class AttackOfOpportunity extends AbstractBackstabCard {
 
     private static final int DAMAGE = 16;
 
-
     private static final int MAGIC = 1; // Card draw
     private static final int UPGRADED_PLUS_MAGIC = 1;
 
     private static final int BACKSTAB = 2; // Voids
     private static final int UPGRADED_PLUS_BACKSTAB = -1;
 
-// /STAT DECLARATION/
+
+    // /STAT DECLARATION/
+
 
     public AttackOfOpportunity() {
         super(ID, NAME, IMG, COST, DESCRIPTION, TYPE, COLOR, RARITY, TARGET);
@@ -64,6 +70,8 @@ public class AttackOfOpportunity extends AbstractBackstabCard {
         this.baseDamage = DAMAGE;
         this.magicNumber = this.baseMagicNumber = MAGIC;
         this.backstabNumber = this.baseBackstabNumber = BACKSTAB;
+
+        tags.add(ThiefCardTags.BACKSTAB);
     }
 
     // Actions the card should do.
@@ -77,16 +85,13 @@ public class AttackOfOpportunity extends AbstractBackstabCard {
                 AbstractGameAction.AttackEffect.SLASH_VERTICAL));
 
         // Add voids to your draw pile
-        if (this.backstabNumber != 0) {
-            while (this.backstabNumber-- != 0) {
-                AbstractDungeon.actionManager.addToBottom(new MakeTempCardInDiscardAction(new VoidCard(), 1));
-            }
-            this.backstabNumber = this.baseBackstabNumber;
+        if (backstabNumber != 0) {
+            AbstractDungeon.actionManager.addToBottom(new MakeTempCardInDrawPileAction(new VoidCard(), backstabNumber, true, true));
         }
 
         // Backstab - Draw 1 card.
         if (count <= 1) {
-            AbstractDungeon.actionManager.addToBottom(new DrawCardAction(p, this.magicNumber));
+            AbstractDungeon.actionManager.addToBottom(new DrawCardAction(p, magicNumber));
         }
 
     }
@@ -94,10 +99,34 @@ public class AttackOfOpportunity extends AbstractBackstabCard {
     @Override
     public void applyPowers() {
         super.applyPowers();
-        if (AbstractDungeon.player.cardsPlayedThisTurn == 0) {
-            this.rawDescription = this.DESCRIPTION + this.EXTENDED_DESCRIPTION[1];
+        if (backstabNumber == 1) {
+            if (magicNumber == 1) {
+                if (AbstractDungeon.player.cardsPlayedThisTurn == 0 || AbstractDungeon.player.hasPower(BackstabPower.POWER_ID)) {
+                    rawDescription = EXTENDED_DESCRIPTION[1] + EXTENDED_DESCRIPTION[3];
+                } else {
+                    rawDescription = EXTENDED_DESCRIPTION[1] + EXTENDED_DESCRIPTION[4];
+                }
+            } else {
+                if (AbstractDungeon.player.cardsPlayedThisTurn == 0 || AbstractDungeon.player.hasPower(BackstabPower.POWER_ID)) {
+                    rawDescription = EXTENDED_DESCRIPTION[1] + EXTENDED_DESCRIPTION[5];
+                } else {
+                    rawDescription = EXTENDED_DESCRIPTION[1] + EXTENDED_DESCRIPTION[6];
+                }
+            }
         } else {
-            this.rawDescription = this.DESCRIPTION + this.EXTENDED_DESCRIPTION[2];
+            if (magicNumber == 1) {
+                if (AbstractDungeon.player.cardsPlayedThisTurn == 0 || AbstractDungeon.player.hasPower(BackstabPower.POWER_ID)) {
+                    rawDescription = EXTENDED_DESCRIPTION[2] + EXTENDED_DESCRIPTION[3];
+                } else {
+                    rawDescription = EXTENDED_DESCRIPTION[2] + EXTENDED_DESCRIPTION[4];
+                }
+            } else {
+                if (AbstractDungeon.player.cardsPlayedThisTurn == 0 || AbstractDungeon.player.hasPower(BackstabPower.POWER_ID)) {
+                    rawDescription = EXTENDED_DESCRIPTION[2] + EXTENDED_DESCRIPTION[5];
+                } else {
+                    rawDescription = EXTENDED_DESCRIPTION[2] + EXTENDED_DESCRIPTION[6];
+                }
+            }
         }
         this.initializeDescription();
     }
@@ -109,12 +138,6 @@ public class AttackOfOpportunity extends AbstractBackstabCard {
         return tips;
     }
 
-    // Which card to return when making a copy of this card.
-    @Override
-    public AbstractCard makeCopy() {
-        return new AttackOfOpportunity();
-    }
-
     //Upgraded stats.
     @Override
     public void upgrade() {
@@ -122,7 +145,6 @@ public class AttackOfOpportunity extends AbstractBackstabCard {
             this.upgradeName();
             this.upgradeMagicNumber(UPGRADED_PLUS_MAGIC);
             this.upgradeBackstabNumber(UPGRADED_PLUS_BACKSTAB);
-//          this.rawDescription = UPGRADE_DESCRIPTION;
             this.initializeDescription();
         }
     }
