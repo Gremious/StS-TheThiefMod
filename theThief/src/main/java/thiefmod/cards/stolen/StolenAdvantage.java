f4f7ce27a51420e5275411b88d8921b061159ecf
Gremious <gremious@protonmail.com>
2019-01-24 18:57:24 +0000

    pom fix

--- theThief/src/main/java/thiefmod/cards/stolen/StolenAdvantage.java
+++ theThief/src/main/java/thiefmod/cards/stolen/StolenAdvantage.java
@@ -2,12 +2,10 @@ package thiefmod.cards.stolen;
 
 import basemod.abstracts.CustomCard;
 import com.megacrit.cardcrawl.actions.common.DrawCardAction;
-import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.localization.CardStrings;
-import com.megacrit.cardcrawl.localization.UIStrings;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import thiefmod.ThiefMod;
 import thiefmod.patches.Unique.ThiefCardTags;
