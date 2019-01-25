abf2e5c611424f0f891f6272361c158c83489a1e
Gremious <gremious@protonmail.com>
2019-01-25 15:11:49 +0000

    Stolen Blood

--- theThief/src/main/java/thiefmod/cards/stolen/StolenBlood.java
+++ theThief/src/main/java/thiefmod/cards/stolen/StolenBlood.java
@@ -1,27 +1,30 @@
 package thiefmod.cards.stolen;
 
-import basemod.BaseMod;
 import basemod.helpers.TooltipInfo;
-import com.megacrit.cardcrawl.actions.common.DrawCardAction;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.localization.CardStrings;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.StrengthPower;
 import thiefmod.ThiefMod;
-import thiefmod.actions.unique.StolenArsenalAction;
 import thiefmod.cards.AbstractBackstabCard;
 import thiefmod.patches.Unique.ThiefCardTags;
+import thiefmod.powers.Unique.SimilarSkillsPower;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class StolenArsenal extends AbstractBackstabCard {
+public class StolenBlood extends AbstractBackstabCard {
 
 
     // TEXT DECLARATION
 
-    public static final String ID = ThiefMod.makeID("StolenArsenal");
+    public static final String ID = ThiefMod.makeID("StolenBlood");
     private static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
 
     public static final String IMG = ThiefMod.makePath(ThiefMod.DEFAULT_COMMON_ATTACK);
@@ -45,12 +48,16 @@ public class StolenArsenal extends AbstractBackstabCard {
     private static final int UPGRADE_COST = 1;
 
 
+    private static final int MAGIC = 8;
+
     // /STAT DECLARATION/
 
 
-    public StolenArsenal() {
+    public StolenBlood() {
         super(ID, NAME, IMG, COST, DESCRIPTION, TYPE, COLOR, RARITY, TARGET);
         this.exhaust = true;
+        this.magicNumber = this.baseMagicNumber = MAGIC;
+
 
         tags.add(ThiefCardTags.STOLEN);
 
@@ -59,19 +66,8 @@ public class StolenArsenal extends AbstractBackstabCard {
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
 
-        int maximumHand = BaseMod.MAX_HAND_SIZE;
-        int currentHand = p.hand.group.size();
-
-        AbstractDungeon.actionManager.addToBottom(new StolenArsenalAction(p));
-
-        do {
-            if (!AbstractDungeon.player.drawPile.isEmpty() || !AbstractDungeon.player.discardPile.isEmpty()) {
-                AbstractDungeon.actionManager.addToBottom(new DrawCardAction(p, 1));
-                currentHand++;
-            } else return;
-        }
-        while (currentHand < maximumHand);
-
+        AbstractDungeon.actionManager.addToBottom(
+                new ApplyPowerAction(p, p, new StrengthPower(p, this.magicNumber), this.magicNumber));
 
     }
 
@@ -89,7 +85,6 @@ public class StolenArsenal extends AbstractBackstabCard {
 
             upgradeBaseCost(UPGRADE_COST);
 
-//          rawDescription = UPGRADE_DESCRIPTION;
             this.initializeDescription();
         }
     }
