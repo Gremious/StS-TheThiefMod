ec2e68e958932bed21e03fa08a85caa3ba43349d
Gremious <gremious@protonmail.com>
2019-02-06 17:11:27 +0000

    Additional changes to draft. Savestate commit.

--- theThief/src/main/java/thiefmod/events/LouseAbuse.java
+++ theThief/src/main/java/thiefmod/events/LouseAbuse.java
@@ -1,12 +1,13 @@
 package thiefmod.events;
 
 public class LouseAbuse {
-    // A random shaky stranger stops you. He asks if you'd be agree to be hired to get rid of a "problem"
-    // "I just...i can't deal with those things man...."
+    // A random shaky stranger stops you. He is shaking and asks if you'd be agree to be hired to get rid of a "problem".
+    // "I just...I can't deal with those things man...."
     // He's talking about louses.
 
     // "[Agree.] I feel you."
     // "[Agree.] Yeah man I totally agree."
     // "[Agree.] Screw those things!"
-    // [Denial.] Punch him in the face. Lose 3 hp.
+        // Gain a unique relic. If you ever FTK a louse - the relic stops glowing and you get a 150g reward.
+    // "[Denial.] Punch him in the face. Lose 3 hp, cause he punches back."
 }
--- theThief/src/main/java/thiefmod/events/Masquerade.java
+++ theThief/src/main/java/thiefmod/events/Masquerade.java
@@ -4,7 +4,7 @@ public class Masquerade {
     // Act 2 event.
 
     // As you climb up a floor, you stumble upon what you instantly recognize as a...formal event?
-    // The floor is lit.
+    // The floor is lit. // Add the image layers 1 by 1. It's all on a new screen
     // The masks are on.
     // The gremlin band is playing.
     // This is a Masquarade Ball!
@@ -13,7 +13,7 @@ public class Masquerade {
     // [Gossip.]
         // You get yourself a small glass of juice and lean against the table, next to two gremlins that seem to be in a very interesting conversation.
         // "Can you believe it! And, about the band leader ..."
-        // You hear them out, and there's a small spot where you can intervene.
+        // You hear them out for a while, and spot a small spot where you can intervene.
             // [Join In.] Oh my dear, that is *Nothing*. You know what I heard?...
                 // They both gasp! [shaking]What a hot piece of gossip![shaking] Those rumors will surely last! At least, as long as the ball does.
                 // Add a 'Hot Gossip!' colorless card to your deck that costs 2(1) and stuns everyone, then exhausts.
@@ -22,25 +22,29 @@ public class Masquerade {
                     // ADD A DOUBT TO YOUR DECK.
 
     // [Flirt.]
-    // You lay your eyes on one of the taller creatures and decide try your charm. It seems that the interest is mutual!
-    // As you get to know each other, the coat it's wearing unbuttons on it's own, and they instantly cover themselves up. How scandalous!
-        // [Proceed.] Offer to take them somewhere more private.
-            // You and the taller creature move to an empty room...
-                // [Proceed.]
-                    // The coat comes off ! It was 3 gremlins in a trenchcoat all along!
-                    // They run scram, giggling away. Now everyone at the ball know what happened. ADD 1 SHAME TO YOUR DECK.
-    // Option 2: Maybe not right now.
-        // You decide that it's not in your best interest to indulge in such things. You have more important things to attend to.
-          // Add 'Dedication' colorless card to your deck. -
+      // You lay your eyes on one of the taller creatures and decide try your charm. The more you talk the more it seems that the interest is mutual!
+      // As you get to know each other, the coat it's wearing unbuttons on it's own, and they instantly cover themselves up. How scandalous!
+          // [Proceed.] Offer to take them somewhere more private.
+              // @You and the taller creature move to a dimply-lit, empty room...@
+                  // [Proceed.]
+                      // The coat comes off ! It was 3 gremlins in a trenchcoat all along!
+                      // They scram, giggling away. Now everyone at the ball know what happened. ADD 1 SHAME TO YOUR DECK.
+          //[Maybe not right now.]
+              // As you talk, you take a step back and remember why you were here in the first place.
+                  // [Proceed.]
+                      // You decide that it's not in your best interest to indulge in such things. You have more important things to attend to.
+                      // Add 'Dedication' colorless card to your deck. - 0 mana, deal 5 (7) damage and draw 1 card.
 
     // [Steal.]
-        // You notice a gentlecreature engaged in conversation. He's absolutely not paying attention to his pockets, but taking something could be risky.
-            // [Steal.] You attempt to sneakily pull something out of his pocket...but you are caught by a guard patroling the ballroom! ADD 1 NORMALITY.
+        // You notice a creature in a black suit, heavily engaged in conversation. He's absolutely not paying attention to his pockets, but taking something could still be risky.
+            // [Steal.]
+                // You attempt to sneakily pull something out of his pocket...but you are caught by a guard patroling the ballroom! ADD 1 NORMALITY.
             // [Engage in conversation.]
-                // If you have mod installed - He's talking about money...this is a spire investor! After his friend goes away, you ask him to get started, and he's very glad to help out. Get 1 copy of spire co stock card.
+                // If you have mod installed - He's really engaged in talking about money...this is a spire investor! After his friend goes away, you ask him if he help you get started, and he's very eager to help out. Get 1 copy of spire co stock card.
                 // If you don't, just get gold.
 
     // [Drink Wine.]
-    // Heal 12% max hp. Add a Drunk curse to your deck.
-    // Drunk - When you draw this card, shuffle 2 "Dazed" into your draw and discard piles. Exhaustive 2.
+        // You decide to relax a bit and lean back again the table with the fruity-juice bowl.
+            // Heal 15% max hp. Add a Drunk curse to your deck.
+            // Drunk - When you draw this card, shuffle 2 "Dazed" into your draw and discard piles. Exhaustive 2.
 }
