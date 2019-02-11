adf8ada3db5c4f479ae9dcf8c16666df63fc5146
Gremious <gremious@protonmail.com>
2019-02-11 13:37:11 +0000

    Fleeting guilt and focused power icons

--- theThief/src/main/java/thiefmod/powers/Unique/FocusedPower.java
+++ theThief/src/main/java/thiefmod/powers/Unique/FocusedPower.java
@@ -1,12 +1,14 @@
 package thiefmod.powers.Unique;
 
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.megacrit.cardcrawl.core.AbstractCreature;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.helpers.ImageMaster;
 import com.megacrit.cardcrawl.localization.PowerStrings;
 import com.megacrit.cardcrawl.powers.AbstractPower;
 import thiefmod.ThiefMod;
+import thiefmod.util.TextureLoader;
 
 // Empty Base
 
@@ -18,13 +20,15 @@ public class FocusedPower extends AbstractPower {
     private static final PowerStrings powerStrings = CardCrawlGame.languagePack.getPowerStrings(POWER_ID);
     public static final String NAME = powerStrings.NAME;
     public static final String[] DESCRIPTIONS = powerStrings.DESCRIPTIONS;
-    public static final String IMG = ThiefMod.makePath(ThiefMod.COMMON_POWER);
+    private static final Texture tex84 = TextureLoader.getTexture("thiefmodAssets/images/powers/84/FocusedPower.png");
+    private static final Texture tex32 = TextureLoader.getTexture("thiefmodAssets/images/powers/32/FocusedPower.png");
 
 
     public FocusedPower(final AbstractCreature owner, final AbstractCreature source, final int amount) {
         name = NAME;
         ID = POWER_ID;
-        img = ImageMaster.loadImage(IMG);
+        region128 = new TextureAtlas.AtlasRegion(tex84, 0, 0, 84, 84);
+        region48 = new TextureAtlas.AtlasRegion(tex32, 0, 0, 32, 32);
         type = PowerType.BUFF;
         isTurnBased = false;
 
