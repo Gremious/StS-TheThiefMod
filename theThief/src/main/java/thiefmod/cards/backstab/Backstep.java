46187a6323387d2db77d3050fcf37b665440557c
Gremious <gremious@protonmail.com>
2019-02-16 13:37:02 +0000

    attack of oppertunity and backstep beta art

--- theThief/src/main/java/thiefmod/cards/backstab/Backstep.java
+++ theThief/src/main/java/thiefmod/cards/backstab/Backstep.java
@@ -25,7 +25,7 @@ public class Backstep extends AbstractBackstabCard {
 // TEXT DECLARATION
 
     public static final String ID = ThiefMod.makeID("Backstep");
-    public static final String IMG = "thiefmodAssets/images/cards/generic_beta_cards/purple_attack.png";
+    public static final String IMG = "thiefmodAssets/images/cards/beta/Backstep.png";
     public static final CardColor COLOR = AbstractCardEnum.THIEF_GRAY;
 
     private static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
