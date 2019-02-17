f7e4892892e02ad9b89310828674cf532cdd7189
Gremious <gremious@protonmail.com>
2019-02-17 23:00:24 +0000

    Steal Art Added

--- theThief/src/main/java/thiefmod/cards/Steal.java
+++ theThief/src/main/java/thiefmod/cards/Steal.java
@@ -25,7 +25,7 @@ public class Steal extends CustomCard {
     private static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
     private static final UIStrings uiStrings = CardCrawlGame.languagePack.getUIString("theThief:TooltipNames");
     public static final String FLAVOR_STRINGS[] = uiStrings.TEXT;
-    public static final String IMG = "thiefmodAssets/images/cards/beta/Attack.png";
+    public static final String IMG = "thiefmodAssets/images/cards/Steal.png";
 
     public static final String NAME = cardStrings.NAME;
     public static final String DESCRIPTION = cardStrings.DESCRIPTION;
