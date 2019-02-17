6ecae6f911a8ace28b5d46e3fe85a2cd3edae43d
Gremious <gremious@protonmail.com>
2019-02-17 16:53:21 +0000

    retrieval updates desc

--- theThief/src/main/java/thiefmod/powers/Unique/RetrievalPower.java
+++ theThief/src/main/java/thiefmod/powers/Unique/RetrievalPower.java
@@ -6,7 +6,6 @@ import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.core.AbstractCreature;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
-import com.megacrit.cardcrawl.helpers.ImageMaster;
 import com.megacrit.cardcrawl.localization.PowerStrings;
 import com.megacrit.cardcrawl.powers.AbstractPower;
 import org.apache.logging.log4j.LogManager;
@@ -56,7 +55,7 @@ public class RetrievalPower extends AbstractPower {
     @Override
     public void onAfterCardPlayed(AbstractCard cardPlayed) {
         count++;
-
+        updateDescription();
         if (count == 3) {
             AbstractDungeon.actionManager.addToBottom(new RetrievalAction(returnAmount));
         }
