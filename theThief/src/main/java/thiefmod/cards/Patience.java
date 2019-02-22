ceb8dbd43d332ae57fefcd2e1a26c948f51d9d9c
Gremious <gremious@protonmail.com>
2019-02-22 17:35:07 +0000

    Patience beta art

--- theThief/src/main/java/thiefmod/cards/Patience.java
+++ theThief/src/main/java/thiefmod/cards/Patience.java
@@ -17,7 +17,7 @@ public class Patience extends AbstractBackstabCard {
 // TEXT DECLARATION
 
     public static final String ID = ThiefMod.makeID("Patience");
-    public static final String IMG = "thiefmodAssets/images/cards/beta/Attack.png";
+    public static final String IMG = "thiefmodAssets/images/cards/beta/Patience.png";
     public static final CardColor COLOR = AbstractCardEnum.THIEF_GRAY;
 
     private static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
