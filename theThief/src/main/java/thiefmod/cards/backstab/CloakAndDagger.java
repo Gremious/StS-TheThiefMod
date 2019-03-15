326f8ce9505f6587899b33cf59d50fbf71e6ef4a
Gremious <gremious@protonmail.com>
2019-03-15 15:59:18 +0000

    cloak and dagger beta

--- theThief/src/main/java/thiefmod/cards/backstab/CloakAndDagger.java
+++ theThief/src/main/java/thiefmod/cards/backstab/CloakAndDagger.java
@@ -23,7 +23,7 @@ public class CloakAndDagger extends AbstractBackstabCard {
 // TEXT DECLARATION
 
     public static final String ID = ThiefMod.makeID("CloakAndDagger");
-    public static final String IMG = "thiefmodAssets/images/cards/generic_beta_cards/purple_attack.png";
+    public static final String IMG = "thiefmodAssets/images/cards/beta/CloakAndDagger.png";
     public static final CardColor COLOR = AbstractCardEnum.THIEF_GRAY;
 
     private static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
