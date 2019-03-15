85c7e4c34dcdb64ab20f6a94b07e98b5dcffe8fa
Gremious <gremious@protonmail.com>
2019-03-15 18:21:00 +0000

    ghastly

--- theThief/src/main/java/thiefmod/cards/shadowstep/GhastlyPresence.java
+++ theThief/src/main/java/thiefmod/cards/shadowstep/GhastlyPresence.java
@@ -23,7 +23,7 @@ public class GhastlyPresence extends AbstractBackstabCard {
 // TEXT DECLARATION
 
     public static final String ID = ThiefMod.makeID("GhastlyPresence");
-    public static final String IMG = "thiefmodAssets/images/cards/beta/Attack.png";
+    public static final String IMG = "thiefmodAssets/images/cards/beta/GhastlyPresence.png";
     public static final CardColor COLOR = AbstractCardEnum.THIEF_GRAY;
 
     private static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
