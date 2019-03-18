43d91703c1120248a78bcd82e084aaa445a555e9
Gremious <gremious@protonmail.com>
2019-03-18 00:47:57 +0000

    stolen mystical orb fixeups

--- theThief/src/main/java/thiefmod/actions/unique/stolenMysticalOrbAction.java
+++ theThief/src/main/java/thiefmod/actions/unique/stolenMysticalOrbAction.java
@@ -3,7 +3,6 @@ package thiefmod.actions.unique;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.actions.common.DiscardSpecificCardAction;
 import com.megacrit.cardcrawl.actions.common.ExhaustSpecificCardAction;
-import com.megacrit.cardcrawl.actions.utility.QueueCardAction;
 import com.megacrit.cardcrawl.actions.utility.UnlimboAction;
 import com.megacrit.cardcrawl.actions.utility.WaitAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
@@ -11,11 +10,15 @@ import com.megacrit.cardcrawl.core.Settings;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import mysticmod.MysticMod;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import thiefmod.actions.common.slowerQueueCardAction;
 
 import static mysticmod.patches.MysticTags.IS_ARTE;
 import static mysticmod.patches.MysticTags.IS_SPELL;
 
 public class stolenMysticalOrbAction extends AbstractGameAction {
+    public static final Logger logger = LogManager.getLogger(stolenMysticalOrbAction.class.getName());
     private boolean exhaustCards;
     private AbstractCard card;
     private int times;
@@ -44,7 +47,7 @@ public class stolenMysticalOrbAction extends AbstractGameAction {
             AbstractDungeon.getCurrRoom().souls.remove(card);
             card.freeToPlayOnce = true;
             card.exhaustOnUseOnce = exhaustCards;
-            AbstractDungeon.player.limbo.group.add(card);
+            
             card.current_y = -200.0F * Settings.scale;
             card.target_x = (float) Settings.WIDTH / 2.0F + 200.0F * Settings.scale;
             card.target_y = (float) Settings.HEIGHT / 2.0F;
@@ -53,28 +56,27 @@ public class stolenMysticalOrbAction extends AbstractGameAction {
             card.drawScale = 0.12F;
             card.targetDrawScale = 0.75F;
             
+            AbstractDungeon.player.limbo.group.add(card);
+            
             if (!card.canUse(AbstractDungeon.player, (AbstractMonster) target)) {
                 if (exhaustCards) {
                     AbstractDungeon.actionManager.addToTop(new ExhaustSpecificCardAction(card, AbstractDungeon.player.limbo));
                 } else {
                     AbstractDungeon.actionManager.addToTop(new UnlimboAction(card));
                     AbstractDungeon.actionManager.addToTop(new DiscardSpecificCardAction(card, AbstractDungeon.player.limbo));
-                    AbstractDungeon.actionManager.addToTop(new WaitAction(0.4F));
+                    AbstractDungeon.actionManager.addToTop(new WaitAction(0.1F));
                 }
             } else {
                 card.applyPowers();
-                AbstractDungeon.actionManager.addToTop(new QueueCardAction(card, target));
+                AbstractDungeon.actionManager.addToTop(new slowerQueueCardAction(card, target, true));
                 AbstractDungeon.actionManager.addToTop(new UnlimboAction(card));
-                if (!Settings.FAST_MODE) {
-                    AbstractDungeon.actionManager.addToTop(new WaitAction(Settings.ACTION_DUR_MED));
-                } else {
-                    AbstractDungeon.actionManager.addToTop(new WaitAction(Settings.ACTION_DUR_FASTER));
-                }
             }
             
             i++;
+            logger.info("The card played was:" + card.name);
+            logger.info("played " + i + " out of " + times * 2 + " cards");
             
-            if (i >= times) {
+            if (i >= times * 2) {
                 card = null;
                 tickDuration();
             }
