0de6fb416eb845a6358a17656c87dfafd3314ab9
Gremious <gremious@protonmail.com>
2019-05-15 15:57:25 +0100

    initial abstract setup

--- theThief/src/main/java/thiefmod/cards/abstracts/AbstractStolenCard.java
+++ theThief/src/main/java/thiefmod/cards/abstracts/AbstractStolenCard.java
@@ -1,5 +1,6 @@
 package thiefmod.cards.abstracts;
 
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.localization.UIStrings;
 import thiefmod.CardIgnore;
@@ -10,12 +11,21 @@ public abstract class AbstractStolenCard extends AbstractThiefCard {
     private static final UIStrings uiStrings = CardCrawlGame.languagePack.getUIString("theThief:TooltipNames");
     public static final String[] FLAVOR_STRINGS = uiStrings.TEXT;
     
+    public AbstractStolenCard(final String id, final String img, final int cost, final CardType type, final CardTarget target,
+                              final CardRarity subRarity, final AbstractPlayer.PlayerClass character) {
+        super(id, img, cost, type, CardColor.COLORLESS, CardRarity.SPECIAL, target);
+    
+        setBackgroundTexture("theThiefAssets/images/debug/512CardBG.png", "theThiefAssets/images/debug/1024CardBG.png");
+        setBannerTexture("theThiefAssets/images/debug/512CardBanner.png", "theThiefAssets/images/debug/1024CardBanner.png");
+        setOrbTexture("theThiefAssets/images/debug/SmallEnergyOrb.png", "theThiefAssets/images/debug/LargeEnergyOrb.png");
+    
+    }
+    
     public AbstractStolenCard(final String id, final String img, final int cost, final CardType type, final CardColor color, final CardTarget target) {
         super(id, img, cost, type, color, CardRarity.SPECIAL, target);
-        
     }
-    //==
     
+    //==
     public static String flavortext(String EXTENDED_DESCRIPTION) {
         return EXTENDED_DESCRIPTION;
     }
