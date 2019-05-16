1cc73843b9800e081390876ae88bdcb95d1aedaf
Gremious <gremious@protonmail.com>
2019-05-16 17:04:11 +0100

    Test Card

--- theThief/src/main/java/thiefmod/cards/abstracts/AAAtestCard.java
+++ theThief/src/main/java/thiefmod/cards/abstracts/AAAtestCard.java
@@ -5,12 +5,11 @@ import com.megacrit.cardcrawl.actions.common.MakeTempCardInHandAction;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.localization.CardStrings;
+import com.megacrit.cardcrawl.localization.UIStrings;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import thiefmod.CardIgnore;
 import thiefmod.ThiefMod;
-import thiefmod.actions.common.TestAction;
 import thiefmod.patches.character.AbstractCardEnum;
 
 @Deprecated
@@ -22,8 +21,10 @@ public class AAAtestCard extends AbstractBackstabCard {
     public static final String IMG = "theThiefAssets/images/cards/Test.png";
     public static final CardColor COLOR = AbstractCardEnum.THIEF_GRAY;
     private static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
+    private static final UIStrings uiStrings = CardCrawlGame.languagePack.getUIString("theThief:TooltipNames");
     
     public static final String[] EXTENDED_DESCRIPTION = cardStrings.EXTENDED_DESCRIPTION;
+    public static final String DESCRIPTION = cardStrings.DESCRIPTION;
     
     private static final CardRarity RARITY = CardRarity.SPECIAL;
     private static final CardTarget TARGET = CardTarget.SELF;
@@ -35,7 +36,8 @@ public class AAAtestCard extends AbstractBackstabCard {
     public AAAtestCard() {
         super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
         isInnate = true;
-        exhaust = true;
+        retain = true;
+        purgeOnUse = true;
     }
     
     // Actions the card should do.
@@ -43,17 +45,28 @@ public class AAAtestCard extends AbstractBackstabCard {
     public void use(AbstractPlayer p, AbstractMonster m) {
         logger.info(this.name + " use() start");
         logger.info("Can Backstab: " + canBackstab());
-        if (canBackstab()){
+        if (canBackstab()) {
             logger.info("Triggered backstab action");
             logger.info("Can Backstab: " + canBackstab());
             flash(Color.GREEN);
-        }else{
+        } else {
             logger.info("Triggered non-backstab action");
             logger.info("Can Backstab: " + canBackstab());
             flash(Color.RED);
         }
         
-        act(new MakeTempCardInHandAction(this.makeCopy()));
+        act(new MakeTempCardInHandAction(this.makeStatEquivalentCopy()));
+    }
+    
+    @Override
+    public void applyPowers() {
+        super.applyPowers();
+        if (canBackstab()) {
+            rawDescription = EXTENDED_DESCRIPTION[1] + EXTENDED_DESCRIPTION[2];
+        } else {
+            rawDescription = EXTENDED_DESCRIPTION[1] + EXTENDED_DESCRIPTION[3];
+        }
+        initializeDescription();
     }
     
     @Override
