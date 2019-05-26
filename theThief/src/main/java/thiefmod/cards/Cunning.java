caa47f0004cee326b4312e830f41006fa5c3b2cc
Gremious <gremious@protonmail.com>
2019-05-26 17:02:33 +0300

    Added Cunning beta art

--- theThief/src/main/java/thiefmod/cards/Cunning.java
+++ theThief/src/main/java/thiefmod/cards/Cunning.java
@@ -17,7 +17,7 @@ public class Cunning extends AbstractBackstabCard {
     // TEXT DECLARATION
     
     public static final String ID = ThiefMod.makeID("Cunning");
-    public static final String IMG = "theThiefAssets/images/cards/beta/Attack.png";
+    public static final String IMG = "theThiefAssets/images/cards/beta/Cunning.png";
     public static final CardColor COLOR = AbstractCardEnum.THIEF_GRAY;
     
     private static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
