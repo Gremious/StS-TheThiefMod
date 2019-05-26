34c6b0aa25a6c8b2fe09c9cdff1421857d0b029f
Gremious <gremious@protonmail.com>
2019-05-26 17:51:28 +0300

    Added Sharp Practice Art

--- theThief/src/main/java/thiefmod/cards/SharpPractice.java
+++ theThief/src/main/java/thiefmod/cards/SharpPractice.java
@@ -16,7 +16,7 @@ public class SharpPractice extends AbstractBackstabCard {
     // TEXT DECLARATION
     
     public static final String ID = ThiefMod.makeID("SharpPractice");
-    public static final String IMG = "theThiefAssets/images/cards/beta/Attack.png";
+    public static final String IMG = "theThiefAssets/images/cards/SharpPractice.png";
     public static final CardColor COLOR = AbstractCardEnum.THIEF_GRAY;
     
     private static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
