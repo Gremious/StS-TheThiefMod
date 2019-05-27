6013655bf255960a2db3bbf23fb62a9e4c8fa230
Gremious <gremious@protonmail.com>
2019-05-27 15:21:34 +0300

    Murderous Intent beta art added

--- theThief/src/main/java/thiefmod/actions/common/StealCardAction.java
+++ theThief/src/main/java/thiefmod/actions/common/StealCardAction.java
@@ -13,7 +13,6 @@ import com.megacrit.cardcrawl.helpers.CardLibrary;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import thiefmod.actions.util.DiscoverCardAction;
-import thiefmod.cards.curses.CallOfTheVoid;
 import thiefmod.cards.stolen.*;
 import thiefmod.cards.stolen.modSynergy.bard.StolenCredit;
 import thiefmod.cards.stolen.modSynergy.bard.StolenEssence;
@@ -356,7 +355,7 @@ public class StealCardAction extends AbstractGameAction implements CustomSavable
     
     private void curseCounter() {
         actionManager.addToBottom(new ApplyPowerAction(player, source,
-                new CallOfTheVoid(player, source, 1), 1));
+                new FleetingGuiltPower(player, source, 1), 1));
     }
     
     @Override
