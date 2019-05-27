eadb3b2f30cdcc1f7b3c071b9807a0d8721a3332
Gremious <gremious@protonmail.com>
2019-05-27 12:05:04 +0300

    Multitask beta art added

--- theThief/src/main/java/thiefmod/cards/shadowstep/Multitask.java
+++ theThief/src/main/java/thiefmod/cards/shadowstep/Multitask.java
@@ -22,7 +22,7 @@ public class Multitask extends AbstractBackstabCard {
     // TEXT DECLARATION
     
     public static final String ID = ThiefMod.makeID("Multitask");
-    public static final String IMG = "theThiefAssets/images/cards/beta/Attack.png";
+    public static final String IMG = "theThiefAssets/images/cards/beta/Multitask.png";
     public static final CardColor COLOR = AbstractCardEnum.THIEF_GRAY;
     
     private static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
