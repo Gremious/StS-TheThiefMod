e4c4cea8196a486371622b089aafef057ca76989
Gremious <gremious@protonmail.com>
2019-05-30 16:12:45 +0300

    Shadow Boots loaded art

--- theThief/src/main/java/thiefmod/relics/ShadowBoots.java
+++ theThief/src/main/java/thiefmod/relics/ShadowBoots.java
@@ -11,21 +11,19 @@ import com.megacrit.cardcrawl.helpers.PowerTip;
 public class ShadowBoots extends CustomRelic {
 
     /*
-    at the start of your turn gain 1 energy and shuffle a void into your discard pile
+    At the start of your turn gain 1 energy and shuffle a void into your discard pile
     thanks robbo!
     */
 
     public static final String ID = thiefmod.ThiefMod.makeID("ShadowBoots");
-    public static final String IMG = "theThiefAssets/images/relics/PocketChange.png";
-    public static final String OUTLINE = "theThiefAssets/images/relics/outline/PocketChange.png";
+    public static final String IMG = "theThiefAssets/images/relics/ShadowBoots.png";
+    public static final String OUTLINE = "theThiefAssets/images/relics/outline/ShadowBoots.png";
 
 
     public ShadowBoots() {
         super(ID, ImageMaster.loadImage(IMG), new Texture(OUTLINE), RelicTier.BOSS, LandingSound.MAGICAL);
-
         tips.clear();
         tips.add(new PowerTip(name, description));
-
     }
 
     @Override
