8809bc0bfc9ff5546e727a6ea334968afaf084cb
Gremious <gremious@protonmail.com>
2019-05-31 21:19:16 +0300

    Revert onPreUseCard and rework focused power

--- theThief/src/main/java/thiefmod/hooks/OnPreUseCardPower.java
+++
@@ -1,8 +0,0 @@
-package thiefmod.hooks;
-
-import com.megacrit.cardcrawl.cards.AbstractCard;
-import com.megacrit.cardcrawl.core.AbstractCreature;
-
-public interface OnPreUseCardPower {
-    void onPreUseCard(AbstractCard card, AbstractCreature target);
-}
--- theThief/src/main/java/thiefmod/powers/Unique/FocusedPower.java
+++ theThief/src/main/java/thiefmod/powers/Unique/FocusedPower.java
@@ -2,30 +2,32 @@ package thiefmod.powers.Unique;
 
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.evacipated.cardcrawl.mod.stslib.powers.interfaces.NonStackablePower;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.utility.UseCardAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.core.AbstractCreature;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.localization.PowerStrings;
 import com.megacrit.cardcrawl.powers.AbstractPower;
 import thiefmod.ThiefMod;
 import thiefmod.cards.abstracts.AbstractBackstabCard;
-import thiefmod.hooks.OnPreUseCardPower;
+import thiefmod.powers.Common.BackstabPower;
 import thiefmod.util.TextureLoader;
 
 // Empty Base
 
-public class FocusedPower extends AbstractPower implements OnPreUseCardPower {
+public class FocusedPower extends AbstractPower implements NonStackablePower {
     public AbstractCreature source;
-
-
+    
     public static final String POWER_ID = ThiefMod.makeID("FocusedPower");
     private static final PowerStrings powerStrings = CardCrawlGame.languagePack.getPowerStrings(POWER_ID);
     public static final String NAME = powerStrings.NAME;
     public static final String[] DESCRIPTIONS = powerStrings.DESCRIPTIONS;
     private static final Texture tex84 = TextureLoader.getTexture("theThiefAssets/images/powers/84/FocusedPower.png");
     private static final Texture tex32 = TextureLoader.getTexture("theThiefAssets/images/powers/32/FocusedPower.png");
-
-
+    
     public FocusedPower(final AbstractCreature owner, final AbstractCreature source, final int amount) {
         name = NAME;
         ID = POWER_ID;
@@ -33,28 +35,25 @@ public class FocusedPower extends AbstractPower implements OnPreUseCardPower {
         region48 = new TextureAtlas.AtlasRegion(tex32, 0, 0, 32, 32);
         type = PowerType.BUFF;
         isTurnBased = false;
-
+        
         this.owner = owner;
         this.source = source;
-
+        
         this.amount = amount;
-
+        
         updateDescription();
     }
-
-
+    
+    public void onAfterUseCard(AbstractCard card, UseCardAction action) {
+        if (card instanceof AbstractBackstabCard) {
+            AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(owner, source, new BackstabPower(owner, source, amount), amount));
+        }
+    }
+    
     // Update the description when you apply this power. (i.e. add or remove an "s" in keyword(s))
     @Override
     public void updateDescription() {
         description = DESCRIPTIONS[0];
-    }
-    
-    @Override
-    public void onPreUseCard(AbstractCard card, AbstractCreature target) {
-        if (card instanceof AbstractBackstabCard){
-            ((AbstractBackstabCard) card).canBackstab = true;
-        }
-    
     }
 }
 
